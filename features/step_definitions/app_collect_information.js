0806d91f644c65cc3ca3b3fede13bb23dd3a2479
Scout <ricardog.navi20@gmail.com>
2022-11-07 08:29:36 -0600

    test added

---
+++ features/step_definitions/app_collect_information.js
@@ -0,0 +1,159 @@
+const assert = require("assert");
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+Given("the user is not registred in the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the user click is registred on the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("should be a register in the database", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("the event is full", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the user tries to register", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the user is not registred to the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the user is registred as interested to the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("the user is in the day of the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("is not registred", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("exists availability in the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the user tries to enter to the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the user is registred to the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then(
+  "a flag is added to show the user registred the day of the event",
+  function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+Given("the user is in the day of the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("is not registred", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("not exist availability in the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the user tries to enter to the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the user is registred as interested to the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then(
+  "a flag is added to show the user tried to registred the day of the event",
+  function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+Given("the event has ended", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the user tries to register to the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the user will not be able to register to the event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then(
+  "the user will see a warning saying that the event has already finish",
+  function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+Given("the user unregistred to a certain event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the user click the register button", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then(
+  "the button should be disabled until the request is returned",
+  function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+Given("the user unregistred to a certain event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the user click the register button many times", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the user will be registred only one time", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
