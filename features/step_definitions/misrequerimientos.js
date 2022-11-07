09a6a126835429827ee58e7e29b65aec5ab3c223
Axel <axelca1>
2022-11-07 08:35:27 -0600

    Hola mi test

---
+++ features/step_definitions/misrequerimientos.js
@@ -0,0 +1,82 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+
+         Given('Usuario no se registra en evento.', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+         When('Usuario no registra evento', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+
+         Then('Usuario recibe notificación para registrar', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+
+
+         Given('Usuario registra evento en una hora en específica.', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+
+         When('Usuario registrado.', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+         Then('Usuario no recibe notificaciones en la misma hora.', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+
+         Given('Usuario es parte de la comunidad', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+         When('Usuario no tiene cuenta', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+   
+
+         Then('Usuario generar cuenta temporal', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+         Given('Cuenta de usuario es Alumno', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+
+         When('Usuario no registra ningún evento', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+
+
+         Then('Usuario recibe recomendación de evento', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
