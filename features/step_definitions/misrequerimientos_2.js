d4baa979e564a9af3b36c3aa074a6329f9be040c
Axel <axelca1>
2022-11-14 08:47:56 -0600

    message

---
+++ features/step_definitions/misrequerimientos_2.js
@@ -0,0 +1,53 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('estoy en el registro', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('lleno los campos solicitados', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+
+
+Then('entro a la página de inicio', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+  
+Given('estoy en el registro', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('coloca su correo', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+
+Then('muestra notificación de correo expirado', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+
+Given('estoy en el área de registro', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('es persona exterior al ITESO', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+
+Then('se coloca el correo electrónico', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
