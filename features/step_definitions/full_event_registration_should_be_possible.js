bdd62001e4a505cb6f543c1e79c2e4155a1ff1f8
Scout <ricardog.navi20@gmail.com>
2022-11-23 00:04:42 -0600

    added the tests

---
+++ features/step_definitions/full_event_registration_should_be_possible.js
@@ -0,0 +1,71 @@
+const assert = require("assert");
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+Given("the event is not full", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("the event is not finished", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the user send the registration form", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then(
+  "should be a register in the database with a flag of {string}",
+  function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+Given("the event is not finished", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the user send the registration form", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then(
+  "should be a register in the database with a flag of {string}",
+  function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+Given("the event is not full", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("the event is finished", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the user send the registration form", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then(
+  "should be a register in the database with a flag of {string}",
+  function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+Then("the user should see a modal stating {string}", function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
---
+++ features/step_definitions/user_should_only_be_registred_to_one_event_at_time.js
@@ -0,0 +1,89 @@
+const assert = require("assert");
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+Given("the user is registred into an event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given(
+  "the user is prompted with a modal that ask if the user wants to change the event",
+  function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+When(
+  "the user click the option to stay on the already registred event",
+  function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+Then("the user will not be changed of event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the decision will be stored in a table", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("the user is registred into an event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given(
+  "the user is prompted with a modal that ask if the user wants to change the event",
+  function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+When("the user click the option to change to the new event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the user will be changed of event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the decision will be stored in a table", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("the user is registred into an event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given(
+  "the user is prompted with a modal that ask if the user wants to change the event",
+  function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+When("the user click the option to cancel the request", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the user will not be changed of event", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("the decision will not be stored in a table", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
