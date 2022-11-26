628558496183bf35a39d30176290c3e6fb97c07e
Axel <axelca1>
2022-11-26 08:31:03 -0600

    message

--- features/step_definitions/misrequerimientos_4.js
+++ features/step_definitions/misrequerimientos_4.js
@@ -1,2 +1,17 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('El usuario no esté registrado en el evento.', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+When('El usuario llene el formato de registro', function () {
+// Write code here that turns the phrase above into concrete actions
+return 'pending';
+});
+
+Then('El sistema registra al usuario en una base de datos', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
--- features/step_definitions/misrequerimientos_5.js
+++ features/step_definitions/misrequerimientos_5.js
@@ -1,2 +1,53 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('el usuario no esté registrado en el evento.', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+When('el usuario llene el formato de registro', function () {
+// Write code here that turns the phrase above into concrete actions
+return 'pending';
+});
+
+Then('el sistema registra al usuario en una base de datos', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+Given('Usuario realizó un registro', function () {
+// Write code here that turns the phrase above into concrete actions
+return 'pending';
+});
+
+When('Sistema detecta la fecha del evento {int} horas y {int} hora antes.', function (int, int2) {
+    // When('Sistema detecta la fecha del evento {int} horas y {float} hora antes.', function (int, float) {
+    // When('Sistema detecta la fecha del evento {float} horas y {int} hora antes.', function (float, int) {
+    // When('Sistema detecta la fecha del evento {float} horas y {float} hora antes.', function (float, float2) {
+      // Write code here that turns the phrase above into concrete actions
+      return 'pending';
+    });
+
+Then('Sistema envía una notificación de recordatorio del evento', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+Given('Evento tiene fecha de inicio', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+    });
+
+When('Sistema valida eventos que inician {int} horas antes.', function (int) {
+    // When('Sistema valida eventos que inician {float} horas antes.', function (float) {
+        // Write code here that turns the phrase above into concrete actions
+        return 'pending';
+    });
+
+Then('Sistema notifica al usuario {int} horas antes el inicio del evento', function (int) {
+    // Then('Sistema notifica al usuario {float} horas antes el inicio del evento', function (float) {
+        // Write code here that turns the phrase above into concrete actions
+        return 'pending';
+    });
