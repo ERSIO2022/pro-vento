ca3935ec2e4b070255ae478d6df8fad606e99923
AdriD-12 <111439318+AdriD-12@users.noreply.github.com>
2022-11-27 22:13:00 -0600

    Added own step definitions
    Added own step definitions (further addition of features pending)

---
+++ features/step_definitions/Adri1.js
@@ -0,0 +1,88 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+  When('lleno los campos', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+  Then('entro a la página de inicio', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('las contraseñas no coinciden', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('alerta de error en las contraseñas', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('no se crea la cuenta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('no se verifica el correo', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+  Then('niega la creación de la cuenta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('no se puede crear cuenta', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('estoy en el área de registro', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('es persona exterior al ITESO', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('se coloca el correo', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('coloco un correo que ya registré antes', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('ofrece recuperar contraseña', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('es ex-perteneciente a la comunidad ITESO', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('coloca su correo', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('muestra notificación de correo expirado', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  Given('estoy en el registro', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
